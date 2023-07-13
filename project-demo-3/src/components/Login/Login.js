4ee025d6bc88ff0e372fc46448ee1bc133d99a1a
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-13 19:33:30 -0300

    Using the useEffect Cleanup Function

--- project-demo-3/src/components/Login/Login.js
+++ project-demo-3/src/components/Login/Login.js
@@ -12,9 +12,15 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier);
+    }; // Cleanup function --> It runs every time after a side effect runs
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
