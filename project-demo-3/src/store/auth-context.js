ac068dbedd7a178d5cfe656e69b2404593af77d7
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-14 18:53:35 -0300

    Building & Using a Custom Context Provider Component
    	- This way we can have a more cleaner project
    
    	- Using context for every scneario might not be a good idea
    	- A button per example, if we use a Button Component in a
    Context it means that every time that we use this button component it
    will do exactly the same thing
    
    	- React Context is NOT optimized for high frequency changes -->
    in this case we have redux
    
    	- React Context also shouldn't be used to replace ALL components
    communications and props (Button example)

--- project-demo-3/src/store/auth-context.js
+++ project-demo-3/src/store/auth-context.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+export const AuthContextProvider = (props) => {
+  const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (storedUserLoggedInInformation === "LOGGED_IN") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  const loginHandler = () => {
+    localStorage.setItem("IsLoggedIn", "LOGGED_IN");
+    setIsLoggedIn(true);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
