0978e4242f05442c5b59564c1da2962676a6ed11
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-27 20:59:43 -0300

    Adding new hook and using it on a new form

--- project-demo-9-working-with-forms-and-user-input/src/components/BasicForm.js
+++ project-demo-9-working-with-forms-and-user-input/src/components/BasicForm.js
@@ -1,22 +1,107 @@
+import useInput2 from "./hooks/use-input2";
+
 const BasicForm = (props) => {
+  const {
+    value: nameValue,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameInputBlurHandler,
+    reset: resetName,
+  } = useInput2((value) => value.trim() !== "");
+
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameInputBlurHandler,
+    reset: resetLastName,
+  } = useInput2((value) => value.trim() !== "");
+
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailhangeHandler,
+    inputBlurHandler: emailInputBlurHandler,
+    reset: resetEmail,
+  } = useInput2((value) => value.trim() !== "" && value.trim().includes("@"));
+
+  let formIsValid = false;
+
+  if (nameIsValid && emailIsValid && lastNameIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
+    console.log(emailValue, nameValue, lastNameValue);
+    resetName();
+    resetEmail();
+    resetLastName();
+  };
+
+  const inputNameClasses = nameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const inputEmailClasses = emailHasError
+    ? "form-control invalid"
+    : "form-control";
+  const inputLastNameClasses = lastNameHasError
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="control-group">
+        <div className={inputNameClasses}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            value={nameValue}
+            onChange={nameChangeHandler}
+            onBlur={nameInputBlurHandler}
+          />
+          {nameHasError && (
+            <p className="error-text">Name must not be empty.</p>
+          )}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={inputLastNameClasses}>
+          <label htmlFor="name">Last Name</label>
+          <input
+            type="text"
+            id="name"
+            value={lastNameValue}
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameInputBlurHandler}
+          />
+          {lastNameHasError && (
+            <p className="error-text">Last Name must not be empty.</p>
+          )}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={inputEmailClasses}>
+        <label htmlFor="name">E-Mail Address</label>
+        <input
+          type="text"
+          id="name"
+          value={emailValue}
+          onChange={emailhangeHandler}
+          onBlur={emailInputBlurHandler}
+        />
+        {emailHasError && (
+          <p className="error-text">Please enter a valid email address</p>
+        )}
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
---
+++ project-demo-9-working-with-forms-and-user-input/src/components/hooks/use-input2.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const useInput2 = (validateValue) => {
+  const [value, setValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const isValid = validateValue(value);
+  const hasError = !isValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setValue(event.target.value);
+  };
+
+  const inputBlurHandler = () => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    value,
+    isValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
+
+export default useInput2;
