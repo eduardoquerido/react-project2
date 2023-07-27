8e5696692819c1b9baaa687a635d669639308ee1
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-27 12:27:14 -0300

    Using Custom Hook in more Components

--- project-demo-8-react-hooks-more-realistic-scenario/src/components/NewTask/NewTask.js
+++ project-demo-8-react-hooks-more-realistic-scenario/src/components/NewTask/NewTask.js
@@ -2,40 +2,31 @@ import { useState } from 'react';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createdTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-e8d08-default-rtdb.firebaseio.com/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url: "https://react-http-e8d08-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      // the data used on the applyData will be appended to the end of the params list
+      createdTask.bind(null, taskText) // Bind allows us to pre config a function, it does not trigger the fucntion right away
+    );
   };
 
   return (
