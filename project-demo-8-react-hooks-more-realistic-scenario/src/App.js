889d9db09936cb2915177eb2ed3d4a0e2a4d3266
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-27 12:18:26 -0300

    Adjusting the Custom Hook logic

--- project-demo-8-react-hooks-more-realistic-scenario/src/App.js
+++ project-demo-8-react-hooks-more-realistic-scenario/src/App.js
@@ -1,42 +1,48 @@
-import React, { useEffect, useState } from 'react';
-
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import React, { useEffect, useState } from "react";
+import useHttp from "./hooks/use-http";
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-e8d08-default-rtdb.firebaseio.com/tasks.json"
-      );
+  // FIRST SOLUTION
+  // could also be used with useCallback hook
+  // to guarantee that there is no loop effect for every revalueation
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+  // const transformTasks = useCallback((taskObj) => {
+  //   const loadedTasks = [];
 
-      const data = await response.json();
+  //   for (const taskKey in taskObj) {
+  //     loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+  //   }
 
+  //   setTasks(loadedTasks);
+  // }, []);
+
+  // Then we would pass the trasnformTasks as a parameter for useHttp();
+  // const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
+
+  // SECOND SOLUTION
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (taskObj) => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+    };
 
-  useEffect(() => {
-    fetchTasks();
+    fetchTasks(
+      {
+        url: "https://react-http-e8d08-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
   }, []);
 
   const taskAddHandler = (task) => {
