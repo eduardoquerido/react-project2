80e3bb26ab68e73f8dbbe418a45a6398e36c8f0f
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-27 18:49:52 -0300

    Providing basic validation feedback

--- project-demo-9-working-with-forms-and-user-input/src/components/SimpleInput.js
+++ project-demo-9-working-with-forms-and-user-input/src/components/SimpleInput.js
@@ -1,8 +1,9 @@
 import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
+  // const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -10,6 +11,13 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault(); // do not send the http request to the browser and so don't reload the page
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
 
     console.log(enteredName);
     // const enteredValue = nameInputRef.current.value;
@@ -22,9 +30,13 @@ const SimpleInput = (props) => {
   // if you only interested in reading the value once we should use useRef
   // if we want instant validation we should use useState. Also if we want to reset the enteredName
 
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           value={enteredName}
@@ -32,6 +44,9 @@ const SimpleInput = (props) => {
           id="name"
           onChange={nameInputChangeHandler}
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
