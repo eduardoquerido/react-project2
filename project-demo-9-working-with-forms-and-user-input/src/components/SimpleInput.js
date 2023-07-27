71433b5124aba9f57e16272909a3367ae727c060
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-27 18:42:36 -0300

    Dealing with form submission &  getting user input values

--- project-demo-9-working-with-forms-and-user-input/src/components/SimpleInput.js
+++ project-demo-9-working-with-forms-and-user-input/src/components/SimpleInput.js
@@ -1,9 +1,37 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault(); // do not send the http request to the browser and so don't reload the page
+
+    console.log(enteredName);
+    // const enteredValue = nameInputRef.current.value;
+    // console.log(enteredValue);
+
+    // nameInputRef.current.value = ''; ==> NOT IDEAL< DON'T MANIPULATE THE DOM
+    setEnteredName("");
+  };
+
+  // if you only interested in reading the value once we should use useRef
+  // if we want instant validation we should use useState. Also if we want to reset the enteredName
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          value={enteredName}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
