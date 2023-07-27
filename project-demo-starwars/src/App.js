f93fc21e098058bebbab5324b898d6b574bb60f4
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-26 21:45:48 -0300

    Handling Loading & Data States

--- project-demo-starwars/src/App.js
+++ project-demo-starwars/src/App.js
@@ -5,19 +5,22 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
-    const response = await fetch('https://swapi.dev/api/films/')
-    const data = await response.json();    
-    const transformedMovies = data.results.map(movie => {
+    setIsLoading(true);
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    const transformedMovies = data.results.map((movie) => {
       return {
         id: movie.episode_id,
         title: movie.title,
         openingText: movie.opening_crawl,
-        releaseDate: movie.release_date
-      }
-    })
-    setMovies(transformedMovies)
+        releaseDate: movie.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -26,7 +29,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p> Found no movies. </p>}
+        {isLoading && <p>Loading....</p>}
       </section>
     </React.Fragment>
   );
