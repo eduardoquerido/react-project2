12f125c5a582591dedd538a8b49078c334e028ef
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-29 18:53:02 -0300

    Working with multiple State Properties

--- project-demo-redux/src/store/index.js
+++ project-demo-redux/src/store/index.js
@@ -1,21 +1,38 @@
 import { createStore } from 'redux'
 
-const counterReducer = (state = { counter: 0 }, action) => {
+
+const initialState = { counter: 0, showCounter: true }
+
+const counterReducer = (state = initialState, action) => {
+
+    // Always return a brand new object instead of overwritting the old state
+    // Never overwrite the previousState --> it will work but this is very wrong
+    // this can lead to bugs, unexpected side effects.
     if (action.type === 'INCREMENT_COUNTER') {
         return {
             counter: state.counter + 1,
+            showCounter: state.showCounter
         }
     }
 
     if (action.type === "INCREASE") {
         return {
             counter: state.counter + action.amount,
+            showCounter: state.showCounter
         }
     }
 
     if (action.type === 'DECREMENT_COUNTER'){
         return {
             counter: state.counter - 1,
+            showCounter: state.showCounter
+        }
+    }
+
+    if (action.type === 'TOGGLE'){
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
         }
     }
 
