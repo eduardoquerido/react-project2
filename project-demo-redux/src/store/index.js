4797fefd21f74e5f00ed963164011d3f72bade1d
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-29 19:11:45 -0300

    Introduction to Redux Toolkit and adding state slices

--- project-demo-redux/src/store/index.js
+++ project-demo-redux/src/store/index.js
@@ -1,13 +1,36 @@
 import { createStore } from 'redux'
-
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = { counter: 0, showCounter: true }
+
+
+createSlice({
+    name: 'counterSlice',
+    initialState: initialState,
+    reducers: {
+        increment(state) {
+            // using redux toolkit we kinda of can alter the previous State
+            // becuase redux toolkit will clone the old state.
+            // but keeping in mind, avoiding it is still a good idea
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.amount;
+        },
+        toggle(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
 
 const counterReducer = (state = initialState, action) => {
 
     // Always return a brand new object instead of overwritting the old state
     // Never overwrite the previousState --> it will work but this is very wrong
-    // this can lead to bugs, unexpected side effects.
+    // this can lead to bugs, unexpected side effects. 
     if (action.type === 'INCREMENT_COUNTER') {
         return {
             counter: state.counter + 1,
