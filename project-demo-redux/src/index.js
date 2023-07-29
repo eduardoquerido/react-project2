1ca3606480ee8b9834c6e7f94480cf81f173e2f7
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-29 01:22:41 -0300

    Using Redux Data in the Components

--- project-demo-redux/src/index.js
+++ project-demo-redux/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import { Provider as ReduxProvider } from 'react-redux';
 import store from './store';
 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={store}><App /></Provider>);
+root.render(<ReduxProvider store={store}><App /></ReduxProvider>);
