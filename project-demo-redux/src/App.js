d5453af422b7727caeabfda3fa516c3c841154aa
Eduardo Querido <eduardo.rumbels@gmail.com>
2023-07-29 20:01:32 -0300

    Reading and Dispatching from slices

--- project-demo-redux/src/App.js
+++ project-demo-redux/src/App.js
@@ -1,13 +1,16 @@
-import Counter from './components/Counter';
+import { useSelector } from "react-redux";
+import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
-
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.authSlice.isAuthenticated);
   return (
     <>
       <Header />
-      <Auth />
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <UserProfile />}
       <Counter />
     </>
   );
